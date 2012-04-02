704d18794ddb17177ee58f6fb2f3d4c483f1e8af
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-04-02 06:17:55 +0000

    Added core file analysis to analyze linux core dumps using gdb
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@83 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

---
+++ core/core_analysis.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+import subprocess 
+import glob
+
+bt_dict = {}
+fh = open("gdb_cmds", "w")
+fh.write("bt\n")
+fh.write("quit\n")
+fh.close()
+core_files = glob.glob("core.*")
+for core_file in core_files:
+   print "processing " + core_file
+   backtrace = ""
+   cmd = 'gdb -x "gdb_cmds" -q pdftotext ' + core_file + '| grep "\#"'  
+   p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()[0]
+   tmp = str.split(p, "\n")
+   for line in tmp: 
+      if len(str.split(line, " in ")) >= 2:
+         backtrace = backtrace + str.split(line, " in ")[1]
+    
+   bt_dict[backtrace] = core_file
+print "unique core dumps"
+print len(bt_dict)
