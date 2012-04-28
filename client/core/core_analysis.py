2036ae1308155eb68381a49bfa2870d3c40b4da9
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-04-28 22:47:11 +0000

    refactoring core_analyis.
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@118 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- client/core/core_analysis.py
+++ client/core/core_analysis.py
@@ -3,23 +3,39 @@
 import os
 import subprocess 
 import glob
+import time 
 
-bt_dict = {}
-fh = open("gdb_cmds", "w")
-fh.write("bt\n")
-fh.write("quit\n")
-fh.close()
-core_files = glob.glob("core.*")
-for core_file in core_files:
-    print "processing " + core_file
-    backtrace = ""
-    cmd = 'gdb -x "gdb_cmds" -q pdftotext ' + core_file + '| grep "\#"'  
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()[0]
-    tmp = str.split(p, "\n")
-    for line in tmp: 
-        if len(str.split(line, " in ")) >= 2:
-            backtrace = backtrace + str.split(line, " in ")[1]
+#bt_dict = {}
+#fh = open("gdb_cmds", "w")
+#fh.write("bt\n")
+#fh.write("quit\n")
+#fh.close()
+#core_files = glob.glob("core.*")
+#for core_file in core_files:
+#    print "processing " + core_file
+#    backtrace = ""
+#    cmd = 'gdb -x "gdb_cmds" -q pdftotext ' + core_file + '| grep "\#"'  
+#    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()[0]
+#    tmp = str.split(p, "\n")
+#    for line in tmp: 
+#        if len(str.split(line, " in ")) >= 2:
+#            backtrace = backtrace + str.split(line, " in ")[1]
+#    
+#    bt_dict[backtrace] = core_file
+#print "unique core dumps"
+#print len(bt_dict)
+
+
+LINE_BUFFERED = 1
+
+#NOTE: the first argument is a list
+p = subprocess.Popen(['gdb'], bufsize=LINE_BUFFERED, stdin=subprocess.PIPE)
+for cmd in ["help\n", "help data\n"]:
+    time.sleep(1) # a delay to see that the commands appear one by one
+    p.stdin.write(cmd)
+    # even without .flush() it works as expected on my machine
+p.stdin.close()
+#print p.communicate("help")
+
+#class Core():
     
-    bt_dict[backtrace] = core_file
-print "unique core dumps"
-print len(bt_dict)
