22362ae7b55614e1892ea2e013c873f6964cd27c
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-04-28 22:16:54 +0000

    Added new functions for monitoring directories and processing new core files
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@116 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- client/controller/monitor.py
+++ client/controller/monitor.py
@@ -1,3 +1,58 @@
+import os 
+
 class Monitor:
     def __init__(self, directory):
+        """
+        @param directory: directory that will be monitored
+        """
         self.directory = directory
+        self.files = {}
+        self.old_files = {}
+        self.new_files = []
+    
+    def scan(self):
+        """
+        Get information about a directory (eg. new files placed in the directory). 
+        """
+        self.new_files = []
+        files = os.listdir(self.directory)
+        for file in files: 
+            self.files[file] = True
+        for f in self.files:
+            if not self.old_files.get(f):
+                self.new_files.append(f)
+        self.old_files = self.files
+        
+    def get_new_files(self):
+        """
+        @return: new files in a direcotry
+        """
+        self.scan()
+        return self.new_files
+    
+    def get_core_files(self):
+        """
+        @return: a list of core files that exist in the directory
+        """
+        core_files = []
+        self.scan()
+        for file in self.files:
+            if "core" in str(file):
+                core_files.append(file)
+        return core_files
+    
+    def get_new_core_files(self):
+        """
+        @return: core files that 
+        """
+        core_files = []
+        self.scan()
+        for file in self.new_files:
+            if "core" in str(file):
+                core_files.append(file)
+        return core_files
+    
+if __name__ == "__main__":
+    m = Monitor("./")
+    print m.get_new_files()
+    print m.files
