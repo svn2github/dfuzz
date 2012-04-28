d639d3d829d1c05fd1521f637f9c3d106759cceb
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-04-28 21:55:07 +0000

    changed spaces to 4 spaces
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@115 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- client/core/core_analysis.py
+++ client/core/core_analysis.py
@@ -11,15 +11,15 @@ fh.write("quit\n")
 fh.close()
 core_files = glob.glob("core.*")
 for core_file in core_files:
-   print "processing " + core_file
-   backtrace = ""
-   cmd = 'gdb -x "gdb_cmds" -q pdftotext ' + core_file + '| grep "\#"'  
-   p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()[0]
-   tmp = str.split(p, "\n")
-   for line in tmp: 
-      if len(str.split(line, " in ")) >= 2:
-         backtrace = backtrace + str.split(line, " in ")[1]
+    print "processing " + core_file
+    backtrace = ""
+    cmd = 'gdb -x "gdb_cmds" -q pdftotext ' + core_file + '| grep "\#"'  
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()[0]
+    tmp = str.split(p, "\n")
+    for line in tmp: 
+        if len(str.split(line, " in ")) >= 2:
+            backtrace = backtrace + str.split(line, " in ")[1]
     
-   bt_dict[backtrace] = core_file
+    bt_dict[backtrace] = core_file
 print "unique core dumps"
 print len(bt_dict)
