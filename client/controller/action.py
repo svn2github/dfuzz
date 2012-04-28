bfb6602aca0774d4c79b6d1d2b7126473489d3fd
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-04-28 20:58:11 +0000

    Added a couple functions that to open a cmd, wait and kill the process
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@113 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- client/controller/action.py
+++ client/controller/action.py
@@ -1,4 +1,7 @@
 import os
+import time
+import signal
+import subprocess
 
 class Action():
     """
@@ -13,6 +16,26 @@ class Action():
         pipe = os.popen("su %s" % username, 'w')
         pipe.write(cmd)
 
+    def run_timed(self, cmd, seconds=3):
+        """
+        Run a command that willl be killed in a certain amount of time
+        @param cmd: Command that will be executed
+        @param seconds: seconds to wait before the program is killed
+        """
+        pipe = subprocess.Popen(cmd, shell=True)
+        time.sleep(seconds)
+        if pipe:
+            pipe.kill()
+        
+    def run_timed2(self, cmd, seconds=3):
+        pro = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid) 
+        time.sleep(seconds)
+        os.killpg(pro.pid, signal.SIGTERM)
+        
+    def run(self, cmd):
+        subprocess.call(cmd)
+    
+        
 if __name__ == "__main__":
     a = Action()
-    a.run_as("user_name", "whoami")
+    p = a.run_timed("evince")
