e0f4bb8a548ab2321b48a3139ea956e13ef68d0e
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-04-29 12:33:42 +0000

    Added documentation on how to create public and private keys and how to transfer them to another machine. Changed password to None
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@147 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- server/networking/sftp.py
+++ server/networking/sftp.py
@@ -1,11 +1,17 @@
 import os
 import paramiko
 #yum install python-paramiko
+#http://www.lag.net/paramiko/docs/
 
+
+#ssh-keygen
+#ssh-copy-id 192.168.1.90
+
+
+host = "192.168.1.90"
 ssh = paramiko.SSHClient()
-password = raw_input("enter your password")
 ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
-ssh.connect("192.168.1.90", username="root", password=password)
+ssh.connect(host, username="root", password=None)
 sftp = ssh.open_sftp()
 source = "test.txt"
 destination = "/root/test.txt"
