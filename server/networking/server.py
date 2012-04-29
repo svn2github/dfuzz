3490aee7a625ed58dd76be2fff66f065d567d6fd
blin6j@gmail.com <blin6j@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-04-29 23:16:22 +0000

    sgbhsdlk
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@149 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

---
+++ server/networking/server.py
@@ -0,0 +1,51 @@
+import _mysql
+import SocketServer
+
+class FuzzServer(SocketServer.BaseRequestHandler):
+
+    def handle(self):
+        print "Receiving data from "+self.client_address[0]
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()	
+	    # tokenize received data
+        print "Received "+self.data
+        self.values = self.data.split("|")
+        self.hostid = self.values[0]
+        self.bthash = self.values[1]
+        self.filename = self.values[2]
+        
+        #build query string        
+        self.insertstring = "INSERT INTO CRASHES(hostid,bthash,filename) VALUES ("+self.hostid+",'"+self.bthash+"','"+self.filename+"');"
+        print self.insertstring
+        
+	    #send query to database
+        print "Making a connection to MySQL database"
+        self.dbconnection=_mysql.connect(host="localhost",port=3306,user="dfuzz",passwd="jereSILV0406!(&*",db="DFUZZ") 
+        print "Connected"
+        self.dbconnection.query(self.insertstring)
+        self.dbconnection.close()
+        print "Disconnected"
+if __name__ == "__main__":
+    HOST, PORT = "0.0.0.0", 12456
+
+    # Create the server, binding to localhost on port 12456
+    print "Opening port %i." % PORT
+    server = SocketServer.TCPServer((HOST, PORT), FuzzServer)
+
+    # create local db connection
+    # front end server maintains constant db connection since it is the only 'user'		    	
+    
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
+
+
+
+
+
+
+ 
+
+
+
+
