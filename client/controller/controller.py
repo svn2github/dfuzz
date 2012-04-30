4c03fea55919dc188713a8c2df3963be159c8267
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-04-30 08:35:34 +0000

    Added controller.py. This file can be used to control the client portion of the dfuzz framework
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@159 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

---
+++ client/controller/controller.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+import action
+cwd = os.path.split(os.path.realpath(__file__))[0]
+sys.path.append(os.path.join(cwd, "../config/"))
+sys.path.append(os.path.join(cwd, "../core/"))
+import config
+import core_processer
+
+class Controller():
+    def __init__(self):
+        self.config = config.Config()
+        self.config.parse()
+    
+    def start(self):
+        _action = action.Action()
+        #turn on core dumps
+        _action.run("ulimit -c unlimited")
+        import pdb;pdb.set_trace()
+        trunk = os.path.join(cwd, "../../")
+        os.chdir(os.path.join(trunk,self.config.config["fuzzing_prog_folder"]))
+        fuzz_prog = os.path.split(self.config.config["fuzzing_prog"])[1]
+        fuzz_cmd = "./" + fuzz_prog +" " + self.config.config["fuzzing_prog_args"]
+        os.system(fuzz_cmd)
+        
+if __name__ == "__main__":
+    c = Controller()
+    c.start()
