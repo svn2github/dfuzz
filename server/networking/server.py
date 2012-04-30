ed3ddce5473a74a21fae9669f098679c7b904cca
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-04-30 04:18:40 +0000

    Removed tabs. Added commented code that will help prevent against SQL injection
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@151 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- server/networking/server.py
+++ server/networking/server.py
@@ -7,7 +7,7 @@ class FuzzServer(SocketServer.BaseRequestHandler):
         print "Receiving data from "+self.client_address[0]
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()	
-	    # tokenize received data
+        # tokenize received data
         print "Received "+self.data
         self.values = self.data.split("|")
         self.hostid = self.values[0]
@@ -16,15 +16,25 @@ class FuzzServer(SocketServer.BaseRequestHandler):
         
         #build query string        
         self.insertstring = "INSERT INTO CRASHES(hostid,bthash,filename) VALUES ("+self.hostid+",'"+self.bthash+"','"+self.filename+"');"
+        
+        
         print self.insertstring
         
-	    #send query to database
+        #send query to database
         print "Making a connection to MySQL database"
         self.dbconnection=_mysql.connect(host="localhost",port=3306,user="dfuzz",passwd="jereSILV0406!(&*",db="DFUZZ") 
         print "Connected"
+        
+        #SQL INJECTION PREVENTION
+        ################################################
+        #cursor = self.dbconnection.cursor()
+        #cursor.execute("INSERT INTO CRASHES(hostid,bthash,filename) VALUES (%s,'%s','%s');", (self.hostid,self.bthash,self.filename) )
+        ################################################
+        
         self.dbconnection.query(self.insertstring)
         self.dbconnection.close()
         print "Disconnected"
+        
 if __name__ == "__main__":
     HOST, PORT = "0.0.0.0", 12456
 
