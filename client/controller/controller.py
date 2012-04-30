60f47da5d748f91c5b45d8adfdcf9070d243223d
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-04-30 10:09:54 +0000

    Added monitor control
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@163 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- client/controller/controller.py
+++ client/controller/controller.py
@@ -2,8 +2,9 @@
 
 import os
 import sys
-import subprocess
 import action
+import monitor
+import time
 cwd = os.path.split(os.path.realpath(__file__))[0]
 sys.path.append(os.path.join(cwd, "../config/"))
 sys.path.append(os.path.join(cwd, "../core/"))
@@ -14,6 +15,7 @@ class Controller():
     def __init__(self):
         self.config = config.Config()
         self.config.parse()
+        self.core_p = core_processer.ProcessCores()
     
     def start(self):
         _action = action.Action()
@@ -24,7 +26,21 @@ class Controller():
         fuzz_prog = os.path.split(self.config.config["fuzzing_prog"])[1]
         fuzz_cmd = "./" + fuzz_prog +" " + self.config.config["fuzzing_prog_args"]
         os.system(fuzz_cmd)
-        
+    
+    def start_monitor(self):
+        results_dir = os.path.join(cwd,"../results")
+        m = monitor.DirectoryMonitor(results_dir)
+        _core_processer = core_processer.ProcessCores()
+        while 1:
+            new_files = m.get_new_files()
+            print new_files
+            if new_files:
+                self.core_p.process_core_bts()
+            time.sleep(5)
+            
 if __name__ == "__main__":
     c = Controller()
-    c.start()
+    if len(sys.argv) > 1:
+        c.start_monitor()
+    else:
+        c.start()
