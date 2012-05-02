e4dd5fbecd37be24f9eb933a8578686b807921a3
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-05-02 13:46:36 +0000

    modularized sftp client
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@196 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- server/networking/sftp.py
+++ server/networking/sftp.py
@@ -3,19 +3,31 @@ import paramiko
 #yum install python-paramiko
 #http://www.lag.net/paramiko/docs/
 
-
 #ssh-keygen
 #ssh-copy-id 192.168.1.90
 
+class SFTP():
+    def __init__(self):
+        self.ssh = paramiko.SSHClient()
+        self.ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+        
+    def get(self, host, username, local_file, remote_file):
+        self.ssh.connect(host, username="root", password=None)
+        sftp = self.ssh.open_sftp()
+        sftp.get(remote_file, local_file)
+        sftp.close()
+        self.ssh.close()
 
-host = "192.168.1.90"
-ssh = paramiko.SSHClient()
-ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
-ssh.connect(host, username="root", password=None)
-sftp = ssh.open_sftp()
-local_file = "test.txt"
-remote_file = "/root/test.txt"
-#sftp.put(local_file, remote_file)
-sftp.get(remote_file, local_file)
-sftp.close()
-ssh.close()
+    def put(self, host, username, local_file, remote_file):
+        self.ssh.connect(host, username="root", password=None)
+        sftp = self.ssh.open_sftp()
+        sftp.put(local_file, remote_file)
+        sftp.close()
+        self.ssh.close()
+
+if __name__ == "__main__":        
+    host = "192.168.1.90"
+    local_file = "test.txt"
+    remote_file = "/root/test.txt"
+    s = SFTP()
+    s.put(host, "root", local_file, remote_file)
