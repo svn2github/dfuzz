f41d58a9fdc04c6ff1e58686e3139a365bc7055f
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-05-09 06:35:38 +0000

    Added additional test and documentation
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@231 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- client/test/test_monitor.py
+++ client/test/test_monitor.py
@@ -5,21 +5,37 @@ cwd = os.path.split(os.path.realpath(__file__))[0]
 sys.path.append(os.path.join(cwd, "../controller/"))
 import monitor
 
-class TestConfig(unittest.TestCase):
+class TestMonitor(unittest.TestCase):
 
     def setUp(self):
         """
-        Initalize directory monitor object
+        Initialize directory monitor object
         """
-        core_dir = os.path.join(cwd, "cores")
-        self.directory_monitor = monitor.DirectoryMonitor(core_dir)
+        self.core_dir = os.path.join(cwd, "cores")
+        self.directory_monitor = monitor.DirectoryMonitor(self.core_dir)
+    
+    def tearDown(self):
+        core_1348 = os.path.join(self.core_dir, "core.1348")
+        if os.path.exists(core_1348):
+            os.remove(core_1348)
 
-    def test_parse(self):
+    def test_new_files(self):
         """
+        Test that directory monitor can detect files in a directory
         """
         new_files = self.directory_monitor.get_new_files()
-    	self.assertTrue("core.1345" in new_files)
-    	self.assertTrue("core.1346" in new_files)
+        self.assertTrue("core.1345" in new_files)
+        self.assertTrue("core.1346" in new_files)
 
+    def test_new_file_after_file_added(self):
+        new_files = self.directory_monitor.get_new_files()
+        core_1348 = os.path.join(self.core_dir, "core.1348")
+        cfh = open(core_1348, "w")
+        cfh.write("test")
+        cfh.close()
+        new_files = self.directory_monitor.get_new_files()
+        self.assertTrue(new_files == ["core.1348"])
+        
+        
 if __name__ == '__main__':
     unittest.main()
