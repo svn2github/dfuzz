e211ac55082e38bd5fad8e34773664ad00b0c7c2
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-05-09 08:04:41 +0000

    Added unit test for core processing
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@233 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

---
+++ client/test/test_core_processor.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import unittest
+cwd = os.path.split(os.path.realpath(__file__))[0]
+sys.path.append(os.path.join(cwd, "../core/"))
+import core_processor
+
+class TestCoreProcessor(unittest.TestCase):
+    
+    def setUp(self):
+        """
+        Initalize core processor object
+        """
+        self.core_processor = core_processor.ProcessCores()
+
+    def test_core(self):
+        self.core_processor.process_cores()
+
+
+if __name__ == "__main__":
+    unittest.main()
