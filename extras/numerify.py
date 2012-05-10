9f1f03a36dbcd0e8af7b61d6c4d086a7e8151fe1
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-05-10 08:07:00 +0000

    Added documentation
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@241 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- extras/numerify.py
+++ extras/numerify.py
@@ -1,7 +1,9 @@
 import os
 import sys
+#small script to rename files in a folder to use a consistent name
+#python numerify.py /home/user/pdfs/ 3000000
+
 files2 = os.listdir(sys.argv[1])
-#files2 = sorted(files2,key=int)a
 d = {}
 for file in files2: 
    d[file] = file 
