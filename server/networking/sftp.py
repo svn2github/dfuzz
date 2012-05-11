9e34b34402e863020aa99bdc7a1382afa505d95a
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-05-11 05:14:41 +0000

    Added documentation
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@250 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- server/networking/sftp.py
+++ server/networking/sftp.py
@@ -12,6 +12,13 @@ class SFTP():
         self.ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
         
     def get(self, host, username, local_file, remote_file, password=None):
+        """
+        @param host: IP address of host where file resides 
+        @param username: username on remote machine 
+        @param local_file: local location of where to download the file to 
+        @param remote_file: filename to get on remote machine
+        @param password: password of remote machine (keys are typically used so this will typically be None) 
+        """
         self.ssh.connect(host, username=username, password=password)
         sftp = self.ssh.open_sftp()
         sftp.get(remote_file, local_file)
@@ -19,6 +26,13 @@ class SFTP():
         self.ssh.close()
 
     def put(self, host, username, local_file, remote_file, password=None):
+        """
+        @param host: IP address of remote machine where file is to be transfered 
+        @param username: username on remote machine 
+        @param local_file: file that resides on local macine that is to be transfered
+        @param remote_file: remote location of where file is to be transfered
+        @param password: password of the remote user on the remote machine
+        """
         self.ssh.connect(host, username=username, password=password)
         sftp = self.ssh.open_sftp()
         sftp.put(local_file, remote_file)
