4c7bfb81a7ee675051cef2f5e1e2cd0b57b898a1
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-05-11 04:26:26 +0000

    modularized client
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@246 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- client/networking/client.py
+++ client/networking/client.py
@@ -3,24 +3,56 @@ import sys
 import md5
 import random
 
-SERVER, PORT = "192.160.0.140", 12456
 
-if 1:
+class Client():
+    def __init__(self, server, port):
+        self.server = server
+        self.port = port
+        
+    
+    def send(self, data):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            # Connect to server and send data
+            print "Sending : "+data
+            print "to "+self.server+":"+str(self.port)+"\n"
+            sock.connect((self.server, self.port))
+            sock.sendall(data)
+    
+            # Receive data from the server and shut down
+            #received = sock.recv(1024)
+        finally:
+            sock.close()
+        
+   
+if __name__ == "__main__":
+    c = Client("192.168.1.78", 12456)
+    
+    #generate 
     rand = random.randint(1000000,9999999)
     hash = md5.new(str(rand))
     data = "10001|"+hash.hexdigest()+"|test"+str(rand)+".pdf"
-
-    # Create a socket (SOCK_STREAM means a TCP socket)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    try:
-        # Connect to server and send data
-        print "Sending : "+data
-        print "to "+SERVER+":"+str(PORT)+"\n"
-        sock.connect((SERVER, PORT))
-        sock.sendall(data + "\n")
-
-        # Receive data from the server and shut down
-        #received = sock.recv(1024)
-    finally:
-        sock.close()
+    c.send(data)    
+#
+#SERVER, PORT = "192.160.0.140", 12456
+#
+#if 1:
+#    rand = random.randint(1000000,9999999)
+#    hash = md5.new(str(rand))
+#    data = "10001|"+hash.hexdigest()+"|test"+str(rand)+".pdf"
+#
+#    # Create a socket (SOCK_STREAM means a TCP socket)
+#    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#    
+#    try:
+#        # Connect to server and send data
+#        print "Sending : "+data
+#        print "to "+SERVER+":"+str(PORT)+"\n"
+#        sock.connect((SERVER, PORT))
+#        sock.sendall(data + "\n")
+#
+#        # Receive data from the server and shut down
+#        #received = sock.recv(1024)
+#    finally:
+#        sock.close()
