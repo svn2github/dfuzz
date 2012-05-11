96b6922834cbce553535ecb580b7e9869412ebd3
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-05-11 04:35:41 +0000

    added column to data being sent. removed old commented out code
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@247 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- client/networking/client.py
+++ client/networking/client.py
@@ -28,31 +28,11 @@ class Client():
 if __name__ == "__main__":
     c = Client("192.168.1.78", 12456)
     
-    #generate 
+    #generate rand string to send over the wire
     rand = random.randint(1000000,9999999)
     hash = md5.new(str(rand))
-    data = "10001|"+hash.hexdigest()+"|test"+str(rand)+".pdf"
+    data = "10001|"+hash.hexdigest()+"|test"+str(rand)+".pdf" + "|" + "123808"
     
+    #send data
     c.send(data)    
 #
-#SERVER, PORT = "192.160.0.140", 12456
-#
-#if 1:
-#    rand = random.randint(1000000,9999999)
-#    hash = md5.new(str(rand))
-#    data = "10001|"+hash.hexdigest()+"|test"+str(rand)+".pdf"
-#
-#    # Create a socket (SOCK_STREAM means a TCP socket)
-#    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#    
-#    try:
-#        # Connect to server and send data
-#        print "Sending : "+data
-#        print "to "+SERVER+":"+str(PORT)+"\n"
-#        sock.connect((SERVER, PORT))
-#        sock.sendall(data + "\n")
-#
-#        # Receive data from the server and shut down
-#        #received = sock.recv(1024)
-#    finally:
-#        sock.close()
