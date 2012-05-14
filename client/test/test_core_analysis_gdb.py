19f844b7642ac3fcaa7c433df3c05e484ec6a13e
jake199325@gmail.com <jake199325@gmail.com@925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6>
2012-05-14 06:50:09 +0000

    added unit test to test unique core anlysis that has been integrated with frame tracing
    git-svn-id: http://dfuzz.googlecode.com/svn/trunk@266 925a0dd4-3e1d-fc6f-0c6d-a0c26e5864f6

--- client/test/test_core_analysis_gdb.py
+++ client/test/test_core_analysis_gdb.py
@@ -22,10 +22,23 @@ class TestCoreGDB(unittest.TestCase):
         self.assertTrue(self.core_gdb.gdb)
         
     def test_frame_trace(self):
+        """
+        test core analysis frame tracing technique
+        """
         self.core_gdb.start_gdb()
         self.core_gdb.set_program(self.config.config["fuzzed_program"])
         self.core_gdb.set_core_file("cores/core.10049")
         ft = self.core_gdb.get_frame_trace()
+        self.assertTrue(ft)
+    
+    def test_unique_cores(self):
+        """
+        test core analysis unique core functionality
+        """
+        self.core_gdb.start_gdb()
+        unique_cores = self.core_gdb.get_unique_cores("cores/")
+        self.assertTrue(len(unique_cores)==2)
+        
         
 if __name__ == '__main__':
     unittest.main()        
